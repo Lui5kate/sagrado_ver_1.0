4fb9cb789e88f84686f13dd0a7d1bfe40f3a34b5
Luis Armando Lira <lalg22@outlook.com>
2025-07-11 21:49:47 -0600

    Fix static files configuration for production with WhiteNoise on Render

--- sagrado_cantina/settings.py
+++ sagrado_cantina/settings.py
@@ -82,14 +82,11 @@ WSGI_APPLICATION = 'sagrado_cantina.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': 
+    # 'default': 
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
 
-        dj_database_url.config(
-            default='postgresql://postgres:postgres@localhost/postgres',
-            conn_max_age=600
-        )
+        'default': dj_database_url.config(conn_max_age=600)
 }
 
 
@@ -127,13 +124,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
