2d7890cad4319569e41ff104f70d8e2ef40591b3
Luis Armando Lira <lalg22@outlook.com>
2025-07-11 21:19:09 -0600

    Add working build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-#!/urs/bin/env bash
+#!/usr/bin/env bash
 
 set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
-python manage.y migrate
+python manage.py migrate
