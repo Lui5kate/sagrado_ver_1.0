db867883853ea7a3899a354cfffa565e50d79b53
Luis Armando Lira <lalg22@outlook.com>
2025-07-11 21:26:31 -0600

    Fix database configuration in settings.py using dj_database_url

--- sagrado_cantina/settings.py
+++ sagrado_cantina/settings.py
@@ -82,15 +82,14 @@ WSGI_APPLICATION = 'sagrado_cantina.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default': 
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
 
         dj_database_url.config(
-            default='postgresql://postgres:postgres@loclahost/postgres',
+            default='postgresql://postgres:postgres@localhost/postgres',
             conn_max_age=600
         )
-    }
 }
 
 
